a7a6871a61206dd150c262b8cebbc292ee5be154
sudarsonhalder <sudarsonhalder31@gmail.com>
2020-07-15 01:08:46 +0530

    Day 1:- ques1,2 and 3
    Day 1:- ques1,2 and 3

---
+++ Day1-ques1/index.js
@@ -0,0 +1,28 @@
+// VARIOUS METHOD IN CONSOLE FUNCTION:-
+
+// 1.CONSOLE FOR TIME TAKEN:- 
+
+console.time('Time taken');
+console.timeEnd('Time taken');
+
+// 2.CONSOLE FOR REPRESENTING VARIOUS DATA IN TABLE FORMAT:- 
+
+var a = {name:"JO", age:"26", city:"Mumbai"}
+var b = {name:"DO", age:"26", city:"Mumbai"}
+var c = {name:"GO", age:"27", city:"Mumbai"}
+console.table({a,b,c})
+
+// 3.CONSOLE FOR PRINTING VARIABLE :-
+
+console.log("Hello World")
+
+
+// 5.CONSOLE FOR WARN:-
+
+console.warn("Hello World");
+
+// 6.CONSOLE FOR ERROR:-
+
+console.error("Hello World")
+
+
---
+++ Day1-ques2/index.js
@@ -0,0 +1,48 @@
+// Difference between Variable,Let and Constant:-
+
+////VAR:-///
+
+//string:-
+var name="sudarson";
+console.log(name);
+
+//boolean:-
+var b=false;
+console.log(b);
+
+//number:-
+ var a = 24;
+ console.log(a)
+
+ //array:-
+ var animal=["cat","dog","monkey"];
+ console.log(animal);
+
+
+
+////LET:-///
+ {let student="student name:-"
+     name1 = "JOE"; 
+    name2 = "DOE";
+    console.log(student,name1+","+name2);
+ }
+
+
+////CONST:-///
+const child = "joe";
+console.log(child);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
